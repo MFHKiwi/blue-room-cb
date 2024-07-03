6185259dacd2eae3ac43269119c09349dd30dd46
MFHKiwi <lauri22@online.de>
2024-07-04 11:02:23 +1200

    Added flashlight battery bar

--- flashlight.gd
+++ flashlight.gd
@@ -1,7 +1,24 @@
 extends Node3D
 
+var count_down = 0.25
+
+func _ready():
+	var timer = Timer.new()
+	add_child(timer)
+	timer.wait_time = 0.5
+	timer.autostart = true
+
 func toggle():
 	if $SpotLight3D.visible == true:
 		$SpotLight3D.visible = false
 	else:
-		$SpotLight3D.visible = true
+		if $ProgressBar2.value > 0:
+			$SpotLight3D.visible = true
+		else:
+			pass
+
+func _process(delta):
+	count_down -= delta
+	if count_down <= 0 and $ProgressBar2.value > 0 and $SpotLight3D.visible == true:
+		$ProgressBar2.value -= 1
+		count_down = 0.25
